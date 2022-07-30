b00b206ddebb70efeed697968a89505cce7c2e39
ArtJDev <artemkurilenko1983@yandex.ru>
2022-07-30 13:05:12 +0300

    Remade handlers 30.07.2022

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,12 @@
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,15 +16,12 @@ public class Server {
     private final String POST = "POST";
     private final int THREADS = 64;
     private final List<String> allowedMethods = List.of("GET", "POST");
-    private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     private final Map<String, Map<String, Handler>> allHandlers;
-    private final Map<String, Handler> subHandlers;
     final ExecutorService threadPool;
 
     public Server() {
         this.threadPool = Executors.newFixedThreadPool(THREADS);
         this.allHandlers = new ConcurrentHashMap<>();
-        this.subHandlers = new ConcurrentHashMap<>();
     }
 
     public void listen(int port) {
@@ -34,91 +36,95 @@ public class Server {
     }
 
     public void requestProcess(Socket socket) {
-        try {
-            final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        try (final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())){
             final Request request = parse(socket);
-            if (request.getMethod().equals(GET) && validPaths.contains(request.getPath())) {
-                allHandlers.get(GET).get(request.getPath()).handle(request, out);
+            if (!allHandlers.containsKey(request.getMethod())) {
+                notFound(out);
+                return;
             }
-            if (request.getMethod().equals(POST) && validPaths.contains(request.getPath())) {
-                allHandlers.get(POST).get(request.getPath()).handle(request, out);
+            if (!allHandlers.get(request.getMethod()).containsKey(request.getPath())) {
+                notFound(out);
+                return;
             }
+            allHandlers.get(request.getMethod()).get(request.getPath()).handle(request, out);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     public void addHandler(String method, String path, Handler handler) {
-        subHandlers.put(path, handler);
-        allHandlers.put(method, subHandlers);
+        if (!allHandlers.containsKey(method)) {
+            allHandlers.put(method, new ConcurrentHashMap<>());
+        }
+        allHandlers.get(method).put(path, handler);
     }
 
     public Request parse(Socket socket) throws IOException {
-            final var in = new BufferedInputStream(socket.getInputStream());
-            final var out = new BufferedOutputStream(socket.getOutputStream());
-            final var limit = 4096;
+        final var in = new BufferedInputStream(socket.getInputStream());
+        final var out = new BufferedOutputStream(socket.getOutputStream());
+        final var limit = 4096;
 
-            in.mark(limit);
-            final var buffer = new byte[limit];
-            final var read = in.read(buffer);
-            // ищем request line
-            final var requestLineDelimiter = new byte[]{'\r', '\n'};
-            final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
-            if (requestLineEnd == -1) {
-                badRequest(out);
-                socket.close();
-            }
-            // читаем request line
-            final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
-            if (requestLine.length != 3) {
-                badRequest(out);
-                socket.close();
-            }
-            final var method = requestLine[0];
-            if (!allowedMethods.contains(method)) {
-                badRequest(out);
-                socket.close();
-            }
-            System.out.println(method);
-            final var path = requestLine[1];
-            if (!path.startsWith("/")) {
-                badRequest(out);
-                socket.close();
-            }
-            System.out.println(path);
-            final var protocol = requestLine[2];
-            // ищем заголовки
-            final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
-            final var headersStart = requestLineEnd + requestLineDelimiter.length;
-            final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
-            if (headersEnd == -1) {
-                badRequest(out);
-                socket.close();
-            }
-            // отматываем на начало буфера
-            in.reset();
-            // пропускаем requestLine
-            in.skip(headersStart);
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            badRequest(out);
+            socket.close();
+        }
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            badRequest(out);
+            socket.close();
+        }
+        final var method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+            badRequest(out);
+            socket.close();
+        }
+        System.out.println(method);
+        final var path = requestLine[1];
+        if (!path.startsWith("/")) {
+            badRequest(out);
+            socket.close();
+        }
+        System.out.println(path);
+        final var protocol = requestLine[2];
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            badRequest(out);
+            socket.close();
+        }
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
 
-            final var headersBytes = in.readNBytes(headersEnd - headersStart);
-            final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
-            System.out.println(headers);
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+        System.out.println(headers);
 
-            // для GET тела нет
-            final int length;
-            final byte[] bodyBytes;
-            String body = "";
-            if (!method.equals("GET")) {
-                in.skip(headersDelimiter.length);
-                // вычитываем Content-Length, чтобы прочитать body
-                final var contentLength = extractHeader(headers, "Content-Length");
-                if (contentLength.isPresent()) {
-                    length = Integer.parseInt(contentLength.get());
-                    bodyBytes = in.readNBytes(length);
-                    body = new String(bodyBytes);
-                    System.out.println(body);
-                }
+        // для GET тела нет
+        final int length;
+        final byte[] bodyBytes;
+        String body = "";
+        if (!method.equals("GET")) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers, "Content-Length");
+            if (contentLength.isPresent()) {
+                length = Integer.parseInt(contentLength.get());
+                bodyBytes = in.readNBytes(length);
+                body = new String(bodyBytes);
+                System.out.println(body);
             }
+        }
         return new Request(method, path, protocol, headers, body);
     }
 
@@ -139,6 +145,17 @@ public class Server {
         ).getBytes());
         out.flush();
     }
+
+    private void notFound(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
 
     // from google guava with modifications
     private int indexOf(byte[] array, byte[] target, int start, int max) {
